d600ab97eacad0b107c204cfec83a0df23c3dc7c
Teusner <quentin.brateau@ensta-bretagne.org>
2023-02-03 14:57:38 +0100

    Update

--- plugins/condition/is_pressure_greater_than.cpp
+++ plugins/condition/is_pressure_greater_than.cpp
@@ -42,5 +42,5 @@ namespace riptide_behavior_tree {
 
 #include "behaviortree_cpp_v3/bt_factory.h"
 BT_REGISTER_NODES(factory) {
-    factory.registerNodeType<riptide_behavior_tree::IsPressureGreaterThan>("IsBatteryLowerThan");
+    factory.registerNodeType<riptide_behavior_tree::IsPressureGreaterThan>("IsPressureGreaterThan");
 }
